65cf73b6a6cd63e99c5b60d0efa230feeeacb831
jbben <jbben@LAPTOP-GV4DN158.home>
2022-11-05 17:54:32 +0100

    Code final.

--- src/main/java/ardoisemagique/Main.java
+++ src/main/java/ardoisemagique/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        //on sert du main comme starter
         GraphicalInterface gf = new GraphicalInterface();
         gf.setVisible(true);
     }
